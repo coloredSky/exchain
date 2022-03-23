e53b23babf8804ba887dc211b209ddbf0c93d7cf
zhangkai <zhangkai.gis@163.com>
2022-03-23 18:33:59 +0800

    del useless printf

--- libs/tendermint/mempool/clist_mempool.go
+++ libs/tendermint/mempool/clist_mempool.go
@@ -616,8 +616,7 @@ func (mem *CListMempool) resCbFirstTime(
 				return
 			}
 
-			txHash := r.CheckTx.Tx.TxHash()
-			fmt.Printf("giskook---txhash %v", txHash)
+			//			txHash := r.CheckTx.Tx.TxHash()
 
 			memTx := &mempoolTx{
 				height:      mem.height,
