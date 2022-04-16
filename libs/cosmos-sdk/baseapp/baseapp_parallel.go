cc4224090f35ee54f7e236b77557c8ffff664289
chunfengSun <516108736@qq.com>
2022-04-16 16:25:36 +0800

    Update baseapp_parallel.go

--- libs/cosmos-sdk/baseapp/baseapp_parallel.go
+++ libs/cosmos-sdk/baseapp/baseapp_parallel.go
@@ -549,7 +549,7 @@ func (a *asyncWorkGroup) Start() {
 	if !a.isAsync {
 		return
 	}
-	for index := 0; index < 16; index++ {
+	for index := 0; index < 64; index++ {
 		go func() {
 			for true {
 				select {
